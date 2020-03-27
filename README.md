0cbf571f6ed1b591adab6ac8a2392d44d03464c9
xiaojianpeng <xiaojianpeng@sishuxuefu.com>
2020-03-27 16:15:27 +0800

    增加readme

---
+++ README.md
@@ -0,0 +1,73 @@
+### 使用说明
+
+> 本脚本针对即可时间专栏文章内容，批量下载转为md文档。仅供个人学习使用， **请尊重作者著作权，勿再此售卖获取到专栏文档**。
+
+- 首先  安装依赖
+
+  ```
+  npm install
+  ```
+
+- html转md使用了 `html-to-markdown`库，由于里面转换的格式，存在不生效问题，对转换的`formatters.js`进行了一些修改，所以在使用之前要替换`node_modules`中的 `html-to-markdown`库的`formatters.js`文件
+
+- 若在使用中对格式另有需求，可以修改`formatters.js`文件，替换`node_modules`中的 `html-to-markdown`库的`formatters.js`文件
+
+- 需要修改的参数:
+
+  - 注意Cookie中的SERVERID 中间的时间戳字符串会不定期刷新，这里采用每次请求时使用当前时间戳
+  - loinTime 为账号登录的时间，也可认为是当前用户开始活跃的时间
+
+  ```js
+  // 头信息配置
+  const header_config = {
+    'Content-Type': 'application/json',
+    Host: 'time.geekbang.org'
+  }
+  // 获取文章内容
+  const url1 = 'https://time.geekbang.org/serv/v1/article'
+  //头信息 Referer的url配置
+  const url2 = 'https://time.geekbang.org/column/article'
+  // 获取文章列表
+  const url3 = 'https://time.geekbang.org/serv/v1/column/articles'
+  const Cookie = '_ga=GA1.2.599103124.1569156879; LF_ID=1584327865793-7884681-2455271; GCID=a0c61b9-f229637-f7f5b91-c7d4f37; GRID=a0c61b9-f229637-f7f5b91-c7d4f37; _gid=GA1.2.945582963.1585209103; GCESS=BAYERknztgwBAQUEAAAAAAkBAQMEL198XgcETA50AQEERuIUAAoEAAAAAAIEL198XgsCBAAIAQMEBAAvDQA-; gksskpitn=8ad5546a-c88e-45c4-929f-356a9c2abe7b; Hm_lvt_022f847c4e3acd44d4a2481d9187f1e6=1585215326,1585277185,1585277268; _gat=1; Hm_lpvt_022f847c4e3acd44d4a2481d9187f1e6=1585295036; '
+  
+  let serverId = 'SERVERID=3431a294a18c59fc8f5805662e2bd51e|'
+  const loginTime = '|1585290183;'
+  
+  /**
+   * 获取文章列表所需的body
+   */
+  const list_data = {
+    cid: 48,
+    size: 100,
+    prev: 0,
+    order: "earliest",
+    sample: false
+  }
+  
+  // 获取文章列表 所使用的id
+  const id = '/181'
+  ```
+
+- 运行，在server.js所在目录执行命令
+
+  ```
+  node server.js
+  ```
+
+- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -8,35 +8,49 @@ const {
   log
 } = require('./log4js');
 
-
+//#region 参数
+// 头信息配置
 const header_config = {
   'Content-Type': 'application/json',
   Host: 'time.geekbang.org'
 }
+// 获取文章内容
 const url1 = 'https://time.geekbang.org/serv/v1/article'
+//头信息 Referer的配置
 const url2 = 'https://time.geekbang.org/column/article'
+// 获取文章列表
 const url3 = 'https://time.geekbang.org/serv/v1/column/articles'
-const Cookie = '_ga=GA1.2.599103124.1569156879; LF_ID=1584327865793-7884681-2455271; GCID=a0c61b9-f229637-f7f5b91-c7d4f37; GRID=a0c61b9-f229637-f7f5b91-c7d4f37; _gid=GA1.2.945582963.1585209103; GCESS=BAYERknztgwBAQUEAAAAAAkBAQMEL198XgcETA50AQEERuIUAAoEAAAAAAIEL198XgsCBAAIAQMEBAAvDQA-; gksskpitn=8ad5546a-c88e-45c4-929f-356a9c2abe7b; Hm_lvt_022f847c4e3acd44d4a2481d9187f1e6=1585215326,1585277185,1585277268; Hm_lpvt_022f847c4e3acd44d4a2481d9187f1e6=1585277312; _gat=1;'
+const Cookie = '_ga=GA1.2.599103124.1569156879; LF_ID=1584327865793-7884681-2455271; GCID=a0c61b9-f229637-f7f5b91-c7d4f37; GRID=a0c61b9-f229637-f7f5b91-c7d4f37; _gid=GA1.2.945582963.1585209103; GCESS=BAYERknztgwBAQUEAAAAAAkBAQMEL198XgcETA50AQEERuIUAAoEAAAAAAIEL198XgsCBAAIAQMEBAAvDQA-; gksskpitn=8ad5546a-c88e-45c4-929f-356a9c2abe7b; Hm_lvt_022f847c4e3acd44d4a2481d9187f1e6=1585215326,1585277185,1585277268; _gat=1; Hm_lpvt_022f847c4e3acd44d4a2481d9187f1e6=1585295036; '
 
 let serverId = 'SERVERID=3431a294a18c59fc8f5805662e2bd51e|'
 const loginTime = '|1585290183;'
 
+/**
+ * 获取文章列表所需的body
+ */
+const list_data = {
+  cid: 48,
+  size: 100,
+  prev: 0,
+  order: "earliest",
+  sample: false
+}
 
+// 获取文章列表 所使用的id
+const id = '/181'
+
+//#endregion
+
+//#region 处理文章的函数
 /**
  * 获取文章目录列表
  */
 const get_article_list = async () => {
-  const data = {
-    cid: 48,
-    size: 100,
-    prev: 0,
-    order: 'earliest',
-    sample: false
-  }
-  header_config.Referer = url2 + '/181'
+
+  header_config.Referer = url2 + id
   let SERVERID = serverId + Date.now() + loginTime
   header_config.Cookie = Cookie + SERVERID;
-  var result = await post(url3, data, header_config)
+  var result = await post(url3, list_data, header_config)
   let list = result.data.data.list.map(x => {
     return {
       id: x.id,
@@ -91,9 +105,10 @@ const save = (fileData, filename) => {
     }
   })
 }
+//#endregion
 
 /**
- * 批量任务执行入口
+ * 任务执行入口
  */
 const start = async () => {
   log.info('开始时间：', Date.now())
@@ -102,30 +117,18 @@ const start = async () => {
 
   let promiseeList = []
   list.forEach(x => {
-    if (x.id > 10604) {
+    // 用于目录太多一次获取不完的情况，分多次获取
+    // if (x.id > 5612) {
       promiseeList.push(get_article_content(x.id,
         x.article_title.replace(' ', '').replace('|', '').replace('/', '&')))
-    }
+    // }
   })
   log.info('promiseeList', promiseeList.length)
   Promise.all(promiseeList)
   log.info('结束时间：', Date.now())
 }
 
-// const start = async () => {
-//   log.info('开始时间：', Date.now())
-//   let list = await get_article_list()
-//   log.info('列表获取成功：=========')
+// 单个文章获取
+// get_article_content(12486, '89 程序员练级攻略：UI&UX设计')
 
-//   list.forEach(x => {
-//     setTimeout(() => {
-//       get_article_content(x.id, x.article_title.replace(' ', '').replace('|', ''))
-//     }, 3000);
-
-//   })
-
-//   log.info('结束时间：', Date.now())
-// }
-
-get_article_content(12486, '89 程序员练级攻略：UI&UX设计')
-// start()
+start()
