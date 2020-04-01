d3108c8903f228001be98cbe049be447560b23ed
xiaojianpeng <xiaojianpeng@sishuxuefu.com>
2020-04-01 15:48:03 +0800

    修改头信息

--- server.js
+++ server.js
@@ -11,8 +11,19 @@ const {
 //#region 参数
 // 头信息配置
 const header_config = {
-  'Content-Type': 'application/json',
-  Host: 'time.geekbang.org'
+'Host': 'time.geekbang.org',
+'Connection': 'keep-alive',
+'Content-Length': '58',
+'Accept': 'application/json, text/plain, */*',
+'Sec-Fetch-Dest': 'empty',
+'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36',
+'Content-Type': 'application/json',
+'Origin': 'https://time.geekbang.org',
+'Sec-Fetch-Site': 'same-origin',
+'Sec-Fetch-Mode': 'cors',
+'Referer': 'https://time.geekbang.org/column/article/291',
+'Accept-Encoding': 'gzip, deflate, br',
+'Accept-Language': 'zh-CN,zh;q=0.9',
 }
 // 获取文章内容
 const url1 = 'https://time.geekbang.org/serv/v1/article'
@@ -20,10 +31,10 @@ const url1 = 'https://time.geekbang.org/serv/v1/article'
 const url2 = 'https://time.geekbang.org/column/article'
 // 获取文章列表
 const url3 = 'https://time.geekbang.org/serv/v1/column/articles'
-const Cookie = '_ga=GA1.2.599103124.1569156879; LF_ID=1584327865793-7884681-2455271; GCID=a0c61b9-f229637-f7f5b91-c7d4f37; GRID=a0c61b9-f229637-f7f5b91-c7d4f37; _gid=GA1.2.945582963.1585209103; GCESS=BAYERknztgwBAQUEAAAAAAkBAQMEL198XgcETA50AQEERuIUAAoEAAAAAAIEL198XgsCBAAIAQMEBAAvDQA-; gksskpitn=8ad5546a-c88e-45c4-929f-356a9c2abe7b; Hm_lvt_022f847c4e3acd44d4a2481d9187f1e6=1585215326,1585277185,1585277268; _gat=1; Hm_lpvt_022f847c4e3acd44d4a2481d9187f1e6=1585295036; '
+const Cookie = '_ga=GA1.2.599103124.1569156879; LF_ID=1584327865793-7884681-2455271; GCID=a0c61b9-f229637-f7f5b91-c7d4f37; GRID=a0c61b9-f229637-f7f5b91-c7d4f37; gksskpitn=301ee4bc-60e6-4cdf-893a-0ea30d345016; _gid=GA1.2.395141861.1585707133; _gat=1; GCESS=BAwBAQUEAAAAAAoEAAAAAAgBAwEERuIUAAYERknztgsCBAAHBPXLh1IDBNk1hF4CBNk1hF4EBAAvDQAJAQE-; Hm_lvt_022f847c4e3acd44d4a2481d9187f1e6=1585721094,1585722806,1585722819,1585722843; Hm_lpvt_022f847c4e3acd44d4a2481d9187f1e6=1585722860'
 
-let serverId = 'SERVERID=3431a294a18c59fc8f5805662e2bd51e|'
-const loginTime = '|1585290183;'
+let serverId = 'SERVERID=1fa1f330efedec1559b3abbcb6e30f50|'
+const loginTime = '|1585721038;'
 
 /**
  * 获取文章列表所需的body
@@ -46,18 +57,22 @@ const id = '/181'
  * 获取文章目录列表
  */
 const get_article_list = async () => {
+  try {
+    header_config.Referer = url2 + id
+    let SERVERID = serverId + Date.now() + loginTime
+    header_config.Cookie = Cookie + SERVERID;
+    var result = await post(url3, list_data, header_config)
+    let list = result.data.data.list.map(x => {
+      return {
+        id: x.id,
+        article_title: x.article_title
+      }
+    })
+    return list
+  } catch (error) {
+    console.error('列表异常信息：=========', error)
+  }
 
-  header_config.Referer = url2 + id
-  let SERVERID = serverId + Date.now() + loginTime
-  header_config.Cookie = Cookie + SERVERID;
-  var result = await post(url3, list_data, header_config)
-  let list = result.data.data.list.map(x => {
-    return {
-      id: x.id,
-      article_title: x.article_title
-    }
-  })
-  return list
 }
 
 /**
@@ -82,7 +97,7 @@ const get_article_content = async (id, filename) => {
     }
     log.info('文件保存成功')
   } catch (error) {
-    console.error(error)
+    log.error('异常信息：=========', error)
   }
 
 }
@@ -113,14 +128,13 @@ const save = (fileData, filename) => {
 const start = async () => {
   log.info('开始时间：', Date.now())
   let list = await get_article_list()
-  log.info('列表获取成功：=========')
-
+  log.info('列表获取成功：=========', list)
   let promiseeList = []
   list.forEach(x => {
     // 用于目录太多一次获取不完的情况，分多次获取
     // if (x.id > 5612) {
-      promiseeList.push(get_article_content(x.id,
-        x.article_title.replace(' ', '').replace('|', '').replace('/', '&')))
+    promiseeList.push(get_article_content(x.id,
+      x.article_title.replace(' ', '').replace('|', '').replace('/', '&')))
     // }
   })
   log.info('promiseeList', promiseeList.length)
@@ -129,6 +143,6 @@ const start = async () => {
 }
 
 // 单个文章获取
-// get_article_content(12486, '89 程序员练级攻略：UI&UX设计')
+// get_article_content(291, 'test')
 
 start()
